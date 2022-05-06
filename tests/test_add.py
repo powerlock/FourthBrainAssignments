0c60defd48876623857e2b600cb14afe97d143c5
powerlock <michale.guo002@gmail.com>
2022-05-06 14:39:59 -0700

    add string and array test cases

--- tests/test_add.py
+++ tests/test_add.py
@@ -1,3 +1,11 @@
 from src.add import add
+import numpy as np
 def test_add():
     assert add(1) == 101
+def test_string():
+    assert add("fourth","brain") == "fourthbrain"
+def test_array():
+    a = np.arange(3)
+    b = np.arange(3,6)
+    c = np.array([3,5,7])
+    assert add(a,b).any() == c.any()
